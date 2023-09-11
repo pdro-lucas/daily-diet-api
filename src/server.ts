c12a321bf45b8a0ba5e5bc0973d2954c155dbe16
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-09-11 16:35:03 -0300

    feat: test if app is working with fastify

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import fastify from 'fastify';
+
+const app = fastify();
+
+app.get('/', async (request, reply) => {
+  return 'Hello World';
+});
+
+app.listen(
+  {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+  },
+);
