c39a08090ac92746cb4af3aa1dbb2cb947411e93
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-09-18 15:50:04 -0300

    feat: register meals route

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import fastify from 'fastify';
 import { usersRoutes } from './routes/users';
 import cookie from '@fastify/cookie';
 import { LoginRoute } from './routes/auth';
+import { MealsRoute } from './routes/meals';
 
 const app = fastify();
 
 app.register(cookie);
 app.register(LoginRoute, { prefix: '/auth' });
 app.register(usersRoutes, { prefix: '/users' });
+app.register(MealsRoute, { prefix: '/meals' });
 
 export default app;
