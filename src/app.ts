30c0e165348d0d756044426eb81e73d4f45d6c9f
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-09-18 16:44:55 -0300

    feat: register metrics route

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { usersRoutes } from './routes/users';
 import cookie from '@fastify/cookie';
 import { LoginRoute } from './routes/auth';
 import { MealsRoute } from './routes/meals';
+import { MetricsRoute } from './routes/metrics';
 
 const app = fastify();
 
@@ -10,5 +11,6 @@ app.register(cookie);
 app.register(LoginRoute, { prefix: '/auth' });
 app.register(usersRoutes, { prefix: '/users' });
 app.register(MealsRoute, { prefix: '/meals' });
+app.register(MetricsRoute, { prefix: '/metrics' });
 
 export default app;
